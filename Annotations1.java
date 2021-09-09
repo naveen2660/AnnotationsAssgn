1d137cc06b821a2d872364a89e9e1fc7cd16db3f
unknown <naveen91547@gmail.com>
2021-09-09 12:50:43 +0530

    Annotations assignment

---
+++ Annotations1.java
@@ -0,0 +1,25 @@
+package annot;
+
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+@Target(ElementType.METHOD) 
+@interface Test{
+	
+}
+public class Annotations1 {
+
+	public static void main(String[] args) {
+		printSeries(5);
+
+	}
+
+
+@Test
+static void printSeries(int n){
+    for (int i=0;i<n;i++) {
+    	System.out.println(i);
+    }
+}
+}
